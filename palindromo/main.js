6b9dcff5f85328de62cd72044daa9d94f8e18869
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-21 21:56:42 +0200

    da sistemar le funzioni

--- palindromo/main.js
+++ palindromo/main.js
@@ -4,52 +4,67 @@
 var palindromi = ['otto', 'cane', 'anna', 'benvenuto', 'occorrenza', 'kayak' ];
 
 
+
 //  Inizio il ciclo for per prender ogni parola dall'array
 for (var i = 0; i < palindromi.length; i++) {
     // Inizializzo una variabile di supporto per inidicare ogni singolo elemento che sta scorrendo nel ciclo
     var parolaCorrente = palindromi[i];
-// Creo una variabile con una stringa vuota che viene riempita nel successivo ciclo for da ogni lettera
-    var parola = '';
-// Ciclo for dentro un ciclo for, utilizzato per prender ogni elemento del primo ciclo e scomporlo a sua volta ma partendo dall'utilimo elemento
-    for (var j = parolaCorrente.length - 1; j >= 0; j--) {
-// Unisco ogni lettera dell'elemento che ho scomposto per riformare una la parola ma al contrario
-        parola += parolaCorrente[j]
+    // Creo una variabile con una stringa vuota che viene riempita nel successivo ciclo for da ogni lettera
+        var parola = '';
+    // Ciclo for dentro un ciclo for, utilizzato per prender ogni elemento del primo ciclo e scomporlo a sua volta ma partendo dall'utilimo elemento
+        for (var j = parolaCorrente.length - 1; j >= 0; j--) {
+    // Unisco ogni lettera dell'elemento che ho scomposto per riformare una la parola ma al contrario
+            parola += parolaCorrente[j]
 
-    }
+        }
 
-    // stampo la parola al contrario per intero
-    console.log(parola);
-// Creo la condizione tale per cui, se la parola appena scomposta al contrario è uguale alla parola iniziale del primo ciclo for, prenda la prima strada, altrimenti, la seconda
-    if (parolaCorrente == parola) {
-        console.log('La parola ' + parolaCorrente + ' è palindroma');
-    } else {
-        console.log('La parola ' + parolaCorrente + ' NON è palindroma');
-    }
+        // stampo la parola al contrario per intero
+        console.log(parola);
+    // Creo la condizione tale per cui, se la parola appena scomposta al contrario è uguale alla parola iniziale del primo ciclo for, prenda la prima strada, altrimenti, la seconda
+        if (parolaCorrente == parola) {
+            console.log('La parola ' + parolaCorrente + ' è palindroma');
+        } else {
+            console.log('La parola ' + parolaCorrente + ' NON è palindroma');
+        }
+
 
 }
 
 
 
-function palindrom(stringa) {
 
-
-    var parola = '';
-
-    for (var i = stringa.length -1; i >= 0 ; i--) {
-        parola += stringa[i];
-    }
-
-    if (stringa == parola) {
-        console.log('La parola ' + stringa + ' è palindroma');
-    } else {
-        console.log('La parola ' + stringa + ' NON è palindroma');
-    }
-
-    return;
-
-}
-
-
-var test = palindrom('ciao');
-
-console.log(test);
+// function arrayToElements(arrayName) {
+//
+//     for (var i = 0; i < arrayName.length; i++) {
+//         // Inizializzo una variabile di supporto per inidicare ogni singolo elemento che sta scorrendo nel ciclo
+//         var parolaCorrente = arrayName[i];
+//
+//     }
+//
+//     return parolaCorrente
+// }
+//
+//
+// function palindrom(stringa) {
+//
+//
+//     var parola = '';
+//
+//     for (var i = stringa.length -1; i >= 0 ; i--) {
+//         parola += stringa[i];
+//     }
+//
+//     var parolaPalindroma = stringa == parola;
+//
+//     if (stringa == parola) {
+//         console.log('La parola ' + stringa + ' è palindroma');
+//     } else {
+//         console.log('La parola ' + stringa + ' NON è palindroma');
+//     }
+//     return parolaPalindroma;
+// }
+//
+//
+// var test = palindrom('otto');
+//
+// console.log(test);
