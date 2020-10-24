595e9d58f01c8bdcb0d3f223054657be2c3651e2
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-24 11:24:46 +0200

    Finito snack 4

--- js-jsnack4/main.js
+++ js-jsnack4/main.js
@@ -5,6 +5,29 @@
 
 // Creo i due array con numero di elementi diversi
 
-var firstArray = ['ciao', 2, 'pippo', 'casa', 7, 'paolo', 'pluto', 1, 'cane' 0];
+var firstArray = ['ciao', 'pippo', 'casa', 'paolo', 'pluto', 'cane', 7, 'gatto', 'topo', 1];
 
 var secondArray = [3, 'mario'];
+
+// Creo un ciclo do-while in cui pushare gli elementi generati casualmente nel
+// secondArray fino a che non raggiunge lo stesso numero di elemnti del firstArray
+
+do {
+    var random = getRndInteger(1, 100);
+
+    secondArray.push(random);
+
+} while (secondArray.length != firstArray.length);
+
+console.log(firstArray);
+
+console.log(secondArray);
+
+
+
+
+
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
