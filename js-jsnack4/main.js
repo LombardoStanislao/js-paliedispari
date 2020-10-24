932889e476478aa89c3dbccc164969055d88afdf
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-24 10:42:15 +0200

    Inizio il JS-jsnack 4

---
+++ js-jsnack4/main.js
@@ -0,0 +1,10 @@
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
+
+// Creo i due array con numero di elementi diversi
+
+var firstArray = ['ciao', 2, 'pippo', 'casa', 7, 'paolo', 'pluto', 1, 'cane' 0];
+
+var secondArray = [3, 'mario'];
